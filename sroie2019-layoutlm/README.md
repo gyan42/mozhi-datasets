931d08f72f69368a66b3678069ab2f5ea19e2020
mageswaran1989 <mageswaran1989@gmail.com>
2021-12-20 20:46:38 +0530

    SROIE 2019 dataset with layput information

---
+++ sroie2019-layoutlm/README.md
@@ -0,0 +1,107 @@
+# Receipts Dataset
+
+Official URL: https://rrc.cvc.uab.es/?ch=13&com=downloads
+
+Drive URL: https://drive.google.com/open?id=1ShItNWXyiY1tFDM5W02bceHuJjyeeJl2
+
+Drive 2 Link: https://drive.google.com/file/d/1cD8Y_qzSaZPjMskFIwI7M6TIEz1P59Pd/view?usp=sharing
+
+
+**Colab Notebook** : https://colab.research.google.com/gist/Mageswaran1989/87a04a5064dbc9d59799cad5e9fa8fdd/hftransformerwithcustom-ner-datset.ipynb#scrollTo=9Eq6T2HuTa4h
+
+
+Tasks - ICDAR 2019 Robust Reading Challenge on Scanned Receipts OCR and Information Extraction
+Dataset and Annotations
+
+The dataset will have 1000 whole scanned receipt images. Each receipt image contains around about four key 
+text fields, such as goods name, unit price and total cost, etc. The text annotated in the dataset mainly 
+consists of digits and English characters. An example scanned receipt is shown below:
+
+sample21.jpg
+
+ 
+
+The dataset is split into a training/validation set (“trainval”) and a test set (“test”). The “trainval” 
+set consists of 600 receipt images which will be made available to the participants along with their annotations. 
+The “test” set consists of 400 images, which will be made available a few weeks before the submission deadline.
+
+For receipt OCR task, each image in the dataset is annotated with text bounding boxes (bbox) and the transcript 
+of each text bbox. Locations are annotated as rectangles with four vertices, which are in clockwise order 
+starting from the top. Annotations for an image are stored in a text file with the same file name. 
+The annotation format is similar to that of ICDAR2015 dataset, which is shown below:
+
+```
+x1_1, y1_1,x2_1,y2_1,x3_1,y3_1,x4_1,y4_1, transcript_1
+
+x1_2,y1_2,x2_2,y2_2,x3_2,y3_2,x4_2,y4_2, transcript_2
+
+x1_3,y1_3,x2_3,y2_3,x3_3,y3_3,x4_3,y4_3, transcript_3
+
+…
+```
+
+
+For the information extraction task, each image in the dataset is annotated with a text file with format shown 
+below:
+
+```
+{
+    "company": "STARBUCKS STORE #10208",
+    
+    "date": "14/03/2015",
+    
+    "address": "11302 EUCLID AVENUE, CLEVELAND, OH (216) 229-0749",
+    
+    "total": "4.95",
+}
+```
+
+
+ 
+**Task 1 - Scanned Receipt Text Localisation**
+Task Description
+
+Localizing and recognizing text are conventional tasks that has appeared in many previous competitions, 
+such as the ICDAR Robust Reading Competition (RRC) 2013, ICDAR RRC 2015 and ICDAR RRC 2017 [1][2]. 
+The aim of this task is to accurately localize texts with 4 vertices. The text localization ground truth will be 
+at least at the level of words. Participants will be asked to submit a zip file containing results for all test 
+images.
+
+Evaluation Protocol
+
+As participating teams may apply localization algorithms to locate text at different levels (e.g. text lines), for the evaluation of text localizazation in this task, the methodology based on DetVal will be implemented. The methodology address partly the problem of one-to-many and many to one correspondences of detected texts. In our evaluation protocol mean average precision (mAP) and average recall will be calculated, based on which F1 score will be computed and used for ranking [3].
+
+ 
+**Task 2 - Scanned Receipt OCR**
+Task Description
+
+The aim of this task is to accurately recognize the text in a receipt image. No localisation information is 
+provided, or is required. Instead the participants are required to offer a list of words recognised in the image.
+The task will be restricted to words comprising Latin characters and numbers only.
+
+The ground truth needed to train for this task is the list of words that appear in the transcriptions. 
+In order to obtain the ground truth for this task, you should tokenise all strings splitting on space. 
+For example the string “Date: 12/3/56” should be tokenised “Date:”, “12/3/56”. While the string 
+“Date: 12 / 3 / 56” should be tokenised “Date:” “12”, “/”, “3”, “/”, “56”.
+
+Evaluation Protocol
+
+For the Recognition ranking we will match all words provided by the participants to the words in the ground truth.
+If certain words are repeated in an image, they are expected to also be repeated in the submission results. 
+We will calculate Precision (# or correct matches over the number of detected words) and Recall (# of correct 
+matches over the number of ground truth words) metrics, and will use the F1 score as the final ranking metric.
+
+ 
+**Task 3 - Key Information Extraction from Scanned Receipts**
+Task Description
+
+The aim of this task is to extract texts of a number of key fields from given receipts, and save the texts for 
+each receipt image in a json file with format shown in Figure 3. Participants will be asked to submit a zip 
+file containing results for all test invoice images.
+Evaluation Protocol
+
+For each test receipt image, the extracted text is compared to the ground truth. An extract text is marked as 
+correct if both submitted content and category of the extracted text matches the groundtruth; Otherwise, 
+marked as incorrect. The precision is computed over all the extracted texts of all the test receipt images. 
+F1 score is computed based on precision and recall. F1 score will be used for ranking. 
+
---
+++ sroie2019-layoutlm/sroie2019_layoutlm_dataset.py
@@ -0,0 +1,174 @@
+# Ref: https://github.com/huggingface/datasets/blob/master/datasets/conll2003/conll2003.py
+
+import datasets
+import os
+from pathlib import Path
+from datasets import ClassLabel, DownloadConfig
+
+logger = datasets.logging.get_logger(__name__)
+
+
+_CITATION = ""
+_DESCRIPTION = """\
+
+"""
+
+
+class SROIE2019Config(datasets.BuilderConfig):
+    """BuilderConfig for SROIE2019"""
+
+    def __init__(self, **kwargs):
+        """BuilderConfig for SROIE2019.
+
+        Args:
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(SROIE2019Config, self).__init__(**kwargs)
+
+
+class SROIE2019(datasets.GeneratorBasedBuilder):
+    """SROIE2019 dataset."""
+
+    BUILDER_CONFIGS = [
+        SROIE2019Config(name="SROIE2019", version=datasets.Version("1.0.0"), description="SROIE2019 dataset"),
+    ]
+
+    def __init__(self,
+                 *args,
+                 cache_dir,
+                 url="https://raw.githubusercontent.com/gyan42/mozhi-datasets/main/sroie2019-layoutlm/version1/",
+                 train_file="train.txt",
+                 val_file="valid.txt",
+                 test_file="test.txt",
+                 ner_tags=("company", "date", "address", "total", "O"),
+                 **kwargs):
+        self._ner_tags = ner_tags
+        self._url = url
+        self._train_file = train_file
+        self._val_file = val_file
+        self._test_file = test_file
+        super(SROIE2019, self).__init__(*args, cache_dir=cache_dir, **kwargs)
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "tokens": datasets.Sequence(datasets.Value("string")),
+                    "ner_tags": datasets.Sequence(
+                        datasets.features.ClassLabel(
+                            names=sorted(list(self._ner_tags))
+                        )
+                    )
+                }
+            ),
+            supervised_keys=None,
+            homepage="",
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        urls_to_download = {
+            "train": f"{self._url}{self._train_file}",
+            "dev": f"{self._url}{self._val_file}",
+            "test": f"{self._url}{self._test_file}",
+        }
+        downloaded_files = dl_manager.download_and_extract(urls_to_download)
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": downloaded_files["test"]}),
+        ]
+
+    def _generate_examples(self, filepath):
+        logger.info("⏳ Generating examples from = %s", filepath)
+        with open(filepath, encoding="utf-8") as f:
+            guid = 0
+            tokens = []
+            ner_tags = []
+            bboxes = []
+            for line in f:
+                if line == "" or line == "\n":
+                    if tokens:
+                        yield guid, {
+                            "id": str(guid),
+                            "tokens": tokens,
+                            "ner_tags": ner_tags,
+                        }
+                        guid += 1
+                        tokens = []
+                        ner_tags = []
+                        bboxes = []
+                else:
+                    # SROIE2019 tokens are space separated
+                    splits = line.split(" ")
+                    tokens.append(splits[0])
+                    ner_tags.append(splits[1].strip())
+                    bboxes.append(list(map(lambda xy: int(xy), splits[2].strip())))
+            # last example
+            yield guid, {
+                "id": str(guid),
+                "tokens": tokens,
+                "ner_tags": ner_tags,
+                "bboxes": bboxes
+            }
+
+
+class HFSREIO2019Dataset(object):
+    """
+    """
+    NAME = "HFSREIO2019Dataset"
+
+    def __init__(self):
+        cache_dir = os.path.join(str(Path.home()), '.mozhi')
+        print("Cache directory: ", cache_dir)
+        os.makedirs(cache_dir, exist_ok=True)
+        download_config = DownloadConfig(cache_dir=cache_dir)
+        self._dataset = SROIE2019(cache_dir=cache_dir)
+        print("Cache1 directory: ",  self._dataset.cache_dir)
+        self._dataset.download_and_prepare(download_config=download_config)
+        self._dataset = self._dataset.as_dataset()
+
+    @property
+    def dataset(self):
+        return self._dataset
+
+    @property
+    def labels(self) -> ClassLabel:
+        return self._dataset['train'].features['ner_tags'].feature.names
+
+    @property
+    def id2label(self):
+        return dict(list(enumerate(self.labels)))
+
+    @property
+    def label2id(self):
+        return {v: k for k, v in self.id2label.items()}
+
+    def train(self):
+        return self._dataset['train']
+
+    def test(self):
+        return self._dataset["test"]
+
+    def validation(self):
+        return self._dataset["validation"]
+
+
+if __name__ == '__main__':
+    # config = DownloadConfig(cache_dir=os.path.join(str(Path.home()), '.mozhi'))
+    # dataset = SROIE2019()
+    # dataset.download_and_prepare(download_config=config)
+    dataset = HFSREIO2019Dataset().dataset
+
+    print(dataset['train'])
+    print(dataset['test'])
+    print(dataset['validation'])
+
+    print("List of tags: ", dataset['train'].features['ner_tags'].feature.names)
+
+
+    print("First sample: ", dataset['train'][0])
